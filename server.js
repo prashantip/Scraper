e7f4625a0d20a8f9ac71bfcef3e898dbf5f2a512
prashanti <iawwfte@gmail.com>
2019-10-21 22:46:53 -0400

    server.js was created

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-/*var logger = require("morgan");
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Our scraping tools
@@ -7,15 +7,117 @@ var mongoose = require("mongoose");
 // It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
-*/
+
 
 // Require all models
-//var db = require("./models");
+var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// Routes
+
+app.get("/scrape",function(req, res){
+  axios.get("http://www.bioethics.com/news").then(function(response){
+
+   var $ = cheerio.load(response.data);
+    
+   ("h5").search(function(i, element){
+         var result = {};
+
+         result.title = $(this)
+            .children("a")
+            .text();
+            result.link = $(this)
+            .children("a")
+            .attr("herf");
+
+            // Create a new Article using the `result` object built from scraping
+
+            db.Article.create(result)
+            .then(function(dbArticle) {
+              
+              console.log(dbArticle);
+            })
+            .catch(function(err) {
+            
+              console.log(err);
+            });
+        });
+    
+        // Send a message to the client
+        res.send("Scrape Complete!");
+
+   });
+
+  });
+
+//route for getting Articles from the db
+
+app.get("/articles", function(req,res){
+   
+    db.Article.find()
+
+    .then (function(dbPopulate){
+
+        res.json(dbPopulate);
+    })
+
+    .catch(function(err){
+        res.json(err);
+    });
+});
+
+//route for grabbing a specific Article by ID
+app.get("/articles/:id", function(req, res) {
+    
+    
+    db.Article.findById(req.params.id)
+    .populate("note")
+    .then(function(dbPopulate) {
+      
+      res.json(dbPopulate);
+    })
+    .catch(function(err) {
+    
+      res.json(err);
+    });
+  });
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+    
+    
+    db.Note.create(req.body)
+      .then(function(dbPopulate) {
+        
+        return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbPopulate._id } }, { new: true });
+      })
+      .then(function(dbPopulate) {
+      
+        res.json(dbPopulate);
+      })
+      .catch(function(err) {
+       
+        res.json(err);
+      });
+  });
+  
+
+
+
 
 
 // Start the server
