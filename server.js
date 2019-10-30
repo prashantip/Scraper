f009e4d0e8737b978b0c938a470f8df76efd016e
prashanti <iawwfte@gmail.com>
2019-10-29 20:47:08 -0400

    added dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
-
+require('dotenv').config()
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
