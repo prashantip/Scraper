47abf6f6e5512462d590bb934a9547a2b9a708fc
prashanti <iawwfte@gmail.com>
2019-10-29 22:09:12 -0400

    update server.js

--- server.js
+++ server.js
@@ -42,10 +42,11 @@ app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
   axios.get("https://news.ycombinator.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
 
+    var $ = cheerio.load(response.data);
+    console.log($);
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("td").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
